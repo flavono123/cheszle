004dd7b44896ff601ee8767660e1c114883c1b77
flavono123 <flavono123@gmail.com>
2024-06-23 14:53:16 +0900

    feat: 힌트 이스터에그
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- src/app/components/InstructionSquares.tsx
+++ src/app/components/InstructionSquares.tsx
@@ -1,5 +1,11 @@
 import React, { ReactNode } from 'react'
 
+const hiddenLinkStyle: React.CSSProperties = {
+  color: 'inherit', // 링크의 색상을 주변 텍스트와 동일하게 만듭니다
+  textDecoration: 'none', // 링크의 밑줄을 제거합니다
+  cursor: 'text',
+};
+
 export default function InstuctionSquares(): ReactNode {
   return (
     <>
@@ -17,7 +23,7 @@ export default function InstuctionSquares(): ReactNode {
           fontSize: '0.7em',
           lineHeight: '0.1em', // TODO: shrnik, this is not working
         }}>
-        Use the standard chess movements to get the Black Knight to the black square
+        Use the standard <a href="https://www.youtube.com/shorts/C-oMJUXvGto" style={hiddenLinkStyle}>chess</a> movements to get the Black Knight to the black square
       </span>
     </>
   )
